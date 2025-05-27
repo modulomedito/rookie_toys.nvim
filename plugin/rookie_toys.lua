f5135d8e84bd14048403f8c9e96d893306e606aa
modulomedito <chcchc1995@outlook.com>
2025-05-27 22:07:26 +0800

    Update search

--- plugin/rookie_toys.lua
+++ plugin/rookie_toys.lua
@@ -1,5 +1,6 @@
 local c = require("rookie_toys.c")
 local git = require("rookie_toys.git")
+local search = require("rookie_toys.search")
 
 -- -- User commands
 -- local api = require("rookie_clangd.api")
@@ -19,6 +20,11 @@ local git = require("rookie_toys.git")
 --     api.generate_compile_commands()
 -- end, {})
 
+-- C
+vim.api.nvim_create_user_command("RookieToysCToggleSourceHeader", function()
+    c.toggle_source_header()
+end, {})
+
 -- Git
 vim.api.nvim_create_user_command("RookieToysGitOpenGraph", function()
     git.open_git_graph_all()
@@ -27,7 +33,10 @@ vim.api.nvim_create_user_command("RookieToysGitOpenGraphLocal", function()
     git.open_git_graph_local()
 end, {})
 
--- C
-vim.api.nvim_create_user_command("RookieToysCToggleSourceHeader", function()
-    c.toggle_source_header()
+-- Search
+vim.api.nvim_create_user_command("RookieToysSearchCurrentWord", function()
+    search.grep_word_under_cursor()
+end, {})
+vim.api.nvim_create_user_command("RookieToysSearchLiveGrep", function()
+    search.live_grep()
 end, {})
