1754cfd6fe099ca4c28fed1078a37576de2df2fc
modulomedito <chcchc1995@outlook.com>
2025-05-27 21:52:56 +0800

    Update

--- lua/rookie_toys/git.lua
+++ lua/rookie_toys/git.lua
@@ -2,7 +2,7 @@ local function output_stdout_to_buffer(cmd)
     vim.fn.jobstart(cmd, {
         on_stdout = function(_, data)
             if data then
-                vim.schedule(function() -- Schedule to run safely in the main loop
+                vim.schedule(function()
                     vim.cmd("new")
                     vim.api.nvim_buf_set_lines(0, 0, -1, false, data)
                 end)
@@ -12,7 +12,7 @@ local function output_stdout_to_buffer(cmd)
 end
 
 local function open_git_graph_local()
-    local cmd = '!git log --graph --decorate '
+    local cmd = 'git log --graph --decorate '
     local decorate = '--pretty=format:"%h [%ad] {%an} |%d %s" --date=format-local:"%y-%m-%d %H:%M"'
     cmd = cmd .. decorate
     -- vim.cmd(cmd)
@@ -20,7 +20,7 @@ local function open_git_graph_local()
 end
 
 local function open_git_graph_all()
-    local cmd = '!git log --all --graph --decorate '
+    local cmd = 'git log --all --graph --decorate '
     local decorate = '--pretty=format:"%h [%ad] {%an} |%d %s" --date=format-local:"%y-%m-%d %H:%M"'
     cmd = cmd .. decorate
 
