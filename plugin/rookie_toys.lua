53a6b0b8cdaf10a87eac0cf0e994572e53b44a6f
modulomedito <chcchc1995@outlook.com>
2025-05-27 21:47:13 +0800

    Update

--- plugin/rookie_toys.lua
+++ plugin/rookie_toys.lua
@@ -1,30 +1,33 @@
-if vim.g.rookie_clangd_is_loaded == 1 then
+local M = {}
+
+if M.rookie_clangd_is_loaded == 1 then
     return
+else
+    M.rookie_clangd_is_loaded = 1
 end
-vim.g.rookie_clangd_is_loaded = 1
 
 -- Setup
-require("rookie_clangd").setup()
+-- require("rookie_clangd").setup()
 local git = require("rookie_toys.git")
 local c = require("rookie_toys.c")
 
--- User commands
-local api = require("rookie_clangd.api")
-vim.api.nvim_create_user_command("RookieClangdGenerateCompileCommands", function()
-    api.generate_compile_commands()
-end, {})
-vim.api.nvim_create_user_command("RookieClangdAddDefineSymbol", function()
-    api.add_define_symbol()
-    api.generate_compile_commands()
-end, {})
-vim.api.nvim_create_user_command("RookieClangdRemoveDefineSymbol", function()
-    api.remove_define_symbol()
-    api.generate_compile_commands()
-end, {})
-vim.api.nvim_create_user_command("RookieClangdChoosePreset", function()
-    api.choose_preset()
-    api.generate_compile_commands()
-end, {})
+-- -- User commands
+-- local api = require("rookie_clangd.api")
+-- vim.api.nvim_create_user_command("RookieClangdGenerateCompileCommands", function()
+--     api.generate_compile_commands()
+-- end, {})
+-- vim.api.nvim_create_user_command("RookieClangdAddDefineSymbol", function()
+--     api.add_define_symbol()
+--     api.generate_compile_commands()
+-- end, {})
+-- vim.api.nvim_create_user_command("RookieClangdRemoveDefineSymbol", function()
+--     api.remove_define_symbol()
+--     api.generate_compile_commands()
+-- end, {})
+-- vim.api.nvim_create_user_command("RookieClangdChoosePreset", function()
+--     api.choose_preset()
+--     api.generate_compile_commands()
+-- end, {})
 
 -- Git
 vim.api.nvim_create_user_command("RookieToysGitOpenGraph", function()
@@ -38,3 +41,6 @@ end, {})
 vim.api.nvim_create_user_command("RookieToysCToggleSourceHeader", function()
     c.toggle_source_header()
 end, {})
+
+-- Final
+return M
