de9365b6cf31022e79378d9bfe272a5bcb5d4116
modulomedito <chcchc1995@outlook.com>
2026-04-07 11:18:33 +0800

    feat(plugin): 新增 rk_autocmd 模块用于 Git 提交自动命令
    添加新的 rk_autocmd 模块，为 gitcommit 文件类型设置自动命令。主要功能包括：
    - 设置文本宽度为 100 字符
    - 添加 <leader>f 快捷键执行 PanguAll 格式化
    - 添加 <C-q> 快捷键关闭提交窗口并延迟显示 GitGraph

---
+++ lua/rookie_toys/rk_autocmd.lua
@@ -0,0 +1,35 @@
+local M = {}
+
+function M.setup()
+    local group = vim.api.nvim_create_augroup("RkGitCommit", { clear = true })
+
+    vim.api.nvim_create_autocmd("FileType", {
+        group = group,
+        pattern = "gitcommit",
+        callback = function()
+            -- setlocal textwidth=100
+            vim.opt_local.textwidth = 100
+
+            -- nnoremap <silent><buffer> <leader>f :PanguAll<CR>
+            vim.keymap.set("n", "<leader>f", ":PanguAll<CR>", {
+                silent = true,
+                buffer = true,
+                desc = "GitCommit: PanguAll",
+            })
+
+            -- nnoremap <silent><buffer> <C-q> :q<Bar>call timer_start(1000, {-> execute('RkGitGraph')})<CR>
+            vim.keymap.set("n", "<C-q>", function()
+                vim.cmd("q")
+                vim.defer_fn(function()
+                    vim.cmd("RkGitGraph")
+                end, 200)
+            end, {
+                silent = true,
+                buffer = true,
+                desc = "GitCommit: Close and show GitGraph",
+            })
+        end,
+    })
+end
+
+return M
--- plugin/rookie_toys.lua
+++ plugin/rookie_toys.lua
@@ -27,6 +27,7 @@ require("rookie_toys.rk_gitdiff").setup()
 require("rookie_toys.rk_tabrename").setup()
 require("rookie_toys.rk_c").setup()
 require("rookie_toys.rk_abbr").setup()
+require("rookie_toys.rk_autocmd").setup()
 
 -- local c = require("rookie_toys.c")
 -- local git = require("rookie_toys.git")
