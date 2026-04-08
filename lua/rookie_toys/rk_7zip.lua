1a10e24b11b39dbb7630cca1318bc850d063efb8
modulomedito <chcchc1995@outlook.com>
2026-04-08 14:42:11 +0800

    feat: 添加 7zip 压缩与解压功能模块
    - 新增 rk_7zip.lua 模块，提供 zip 和 unzip 两个核心功能
    - 支持通过命令行参数、NERDTree 或 NvimTree 选择文件路径
    - 自动创建用户命令 RkZip 和 RkUnzip 便于调用
    - 添加默认按键映射 <leader>za 和 <leader>zx 以快速操作
    - 兼容 Windows 和 Unix 系统的命令执行方式

---
+++ lua/rookie_toys/rk_7zip.lua
@@ -0,0 +1,111 @@
+local M = {}
+
+local function get_path(args)
+    if args and args[1] and args[1] ~= "" then
+        return args[1]
+    end
+
+    if vim.bo.filetype == "nerdtree" and vim.g.NERDTreeFileNode then
+        local node = vim.fn["g:NERDTreeFileNode.GetSelected"]()
+        if node and node.path then
+            return node.path:str()
+        end
+    end
+
+    -- Support nvim-tree as well since it's common in modern setups
+    if vim.bo.filetype == "NvimTree" then
+        local lib = pcall(require, "nvim-tree.lib")
+        if lib then
+            local node = require("nvim-tree.lib").get_node_at_cursor()
+            if node and node.absolute_path then
+                return node.absolute_path
+            end
+        end
+    end
+
+    return vim.fn.expand("%:p")
+end
+
+function M.zip(...)
+    local path = get_path({ ... })
+    if not path or path == "" then
+        vim.api.nvim_err_writeln("No path provided")
+        return
+    end
+
+    path = vim.fn.fnamemodify(path, ":p")
+    -- remove trailing slash
+    path = path:gsub("[/\\]$", "")
+
+    local dir = vim.fn.fnamemodify(path, ":h")
+    local name = vim.fn.fnamemodify(path, ":t")
+    local sep = (vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1) and "\\"
+        or "/"
+    local zip_file = dir .. sep .. name .. ".zip"
+
+    local cmd = string.format('7z a "%s" "%s"', zip_file, path)
+    if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
+        vim.cmd('silent !start cmd /c "' .. cmd .. '"')
+    else
+        vim.cmd("silent !" .. cmd .. " &")
+    end
+    print(string.format("Zipping %s to %s...", path, zip_file))
+end
+
+function M.unzip(...)
+    local path = get_path({ ... })
+    if not path or path == "" then
+        vim.api.nvim_err_writeln("No path provided")
+        return
+    end
+
+    path = vim.fn.fnamemodify(path, ":p")
+
+    local dir = vim.fn.fnamemodify(path, ":h")
+    local name_no_ext = vim.fn.fnamemodify(path, ":t:r")
+    local sep = (vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1) and "\\"
+        or "/"
+    local out_dir = dir .. sep .. name_no_ext
+
+    local cmd = string.format('7z x "%s" -o"%s"', path, out_dir)
+    if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
+        vim.cmd('silent !start cmd /c "' .. cmd .. '"')
+    else
+        vim.cmd("silent !" .. cmd .. " &")
+    end
+    print(string.format("Unzipping %s to %s...", path, out_dir))
+end
+
+function M.setup()
+    vim.api.nvim_create_user_command(
+        "RkZip",
+        function(opts)
+            M.zip(opts.args)
+        end,
+        {
+            nargs = "?",
+            complete = "file",
+            desc = "Zip file or directory using 7z",
+        }
+    )
+
+    vim.api.nvim_create_user_command("RkUnzip", function(opts)
+        M.unzip(opts.args)
+    end, { nargs = "?", complete = "file", desc = "Unzip file using 7z" })
+
+    -- Keymaps
+    vim.keymap.set(
+        "n",
+        "<leader>za",
+        "<cmd>RkZip<CR>",
+        { desc = "7z: Zip current file/dir" }
+    )
+    vim.keymap.set(
+        "n",
+        "<leader>zx",
+        "<cmd>RkUnzip<CR>",
+        { desc = "7z: Unzip current file" }
+    )
+end
+
+return M
