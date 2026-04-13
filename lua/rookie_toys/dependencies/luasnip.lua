3c96835372cec03543f70c163333e19079b59369
modulomedito <chcchc1995@outlook.com>
2026-04-13 13:32:47 +0800

    fix: 在Doxygen模板中添加空行以改善格式
    在INCLUDE行后添加空行，使生成的注释块在视觉上更清晰，符合代码风格要求。

--- lua/rookie_toys/dependencies/luasnip.lua
+++ lua/rookie_toys/dependencies/luasnip.lua
@@ -192,6 +192,7 @@ function M.setup()
             "/// @details",
             "",
             "// INCLUDE",
+            "",
             '#include "',
         }),
         f(get_filename_no_ext),
